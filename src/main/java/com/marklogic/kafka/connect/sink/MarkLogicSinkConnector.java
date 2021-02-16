82ba0aa2b7ce7e724d5c05e437b9f3eb301d431b
Chris Box <chris.box@marklogic.com>
2021-02-16 10:04:23 -0500

    Bump the version

--- src/main/java/com/marklogic/kafka/connect/sink/MarkLogicSinkConnector.java
+++ src/main/java/com/marklogic/kafka/connect/sink/MarkLogicSinkConnector.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class MarkLogicSinkConnector extends SinkConnector {
 
-	static final String MARKLOGIC_SINK_CONNECTOR_VERSION = "1.5.5";
+	static final String MARKLOGIC_SINK_CONNECTOR_VERSION = "1.5.6";
 
 	private Map<String, String> config;
 
